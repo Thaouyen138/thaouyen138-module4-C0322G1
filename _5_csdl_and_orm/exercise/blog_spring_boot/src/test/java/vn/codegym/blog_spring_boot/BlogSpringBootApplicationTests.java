5c560d618a1d4e94c8340a2c71d1b595d81f1705
Thaouyen138 <123uyen234@gmail.com>
2022-07-20 19:25:23 +0700

    exercise

---
+++ _5_csdl_and_orm/exercise/blog_spring_boot/src/test/java/vn/codegym/blog_spring_boot/BlogSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.codegym.blog_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ _6_jpa_spring_boot/exercise/blog_spring_boot/src/main/resources/database/blog.sql
@@ -0,0 +1,4 @@
+insert  into blog values
+(1,"truyện tranh","trạng quỷnh tập 11","trạng quỷnh",0, "2022-08-08"),
+(2,"truyện tranh","trạng quỷnh tập 12","trạng quỷnh",0, "2022-09-09"),
+(3,"truyện tranh","trạng quỷnh tập 13","trạng quỷnh",0, "2022-10-10");
