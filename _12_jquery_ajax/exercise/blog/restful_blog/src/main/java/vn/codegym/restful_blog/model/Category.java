65a60eea6aa94d8ac6aa025162edf234bdf1950e
Thaouyen138 <123uyen234@gmail.com>
2022-07-31 22:32:36 +0700

    exer

---
+++ _12_jquery_ajax/exercise/blog/restful_blog/src/main/java/vn/codegym/restful_blog/model/Category.java
@@ -0,0 +1,53 @@
+package vn.codegym.restful_blog.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    private Integer id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    @JsonBackReference
+    private Set<Blog> blogs;
+
+    public Category(Integer id, String name, Set<Blog> blogs) {
+        this.id = id;
+        this.name = name;
+        this.blogs = blogs;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public Set<Blog> getBlogs() {
+        return blogs;
+    }
+
+    public void setBlogs(Set<Blog> blogs) {
+        this.blogs = blogs;
+    }
+}
---
+++ _12_jquery_ajax/exercise/blog/restful_blog/src/main/java/vn/codegym/restful_blog/service/IBlogService.java
@@ -0,0 +1,12 @@
+package vn.codegym.restful_blog.service;
+
+import vn.codegym.restful_blog.model.Blog;
+
+import java.util.List;
+
+public interface IBlogService {
+
+    List<Blog> findAll();
+
+    Blog findbyId(Integer id);
+}
